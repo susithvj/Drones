0f18b3c53e17d287401fa87f53ac0c95119eb8cd
Susith Jayakody <susithj@alliontechnologies.com>
2022-11-05 15:58:02 +0530

    #4 Task004 API - Registering a drone implementation

--- src/main/java/com/musala/drones/controller/DroneController.java
+++ src/main/java/com/musala/drones/controller/DroneController.java
@@ -2,11 +2,12 @@ package com.musala.drones.controller;
 
 import com.musala.drones.controller.api.DroneAPI;
 import com.musala.drones.service.DroneService;
-import dto.DroneDTO;
+import com.musala.drones.dto.DroneDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,10 @@ public class DroneController implements DroneAPI {
     @Override
     public ResponseEntity<List<DroneDTO>> getAllDrones() {
         return new ResponseEntity<>(droneService.getAllDrones(), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<DroneDTO> registerDrone(@Valid DroneDTO droneDTO) {
+        return new ResponseEntity<>(droneService.registerDrone(droneDTO),HttpStatus.OK);
     }
 }
